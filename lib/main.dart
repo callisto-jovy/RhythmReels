90f5fe7d3d8cf32eb38f0f50114279d6415740df
yugata <romanportius@gmail.com>
2024-03-26 19:01:56 +0100

    Move versioning methods into config.dart & add changelog capabilities

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:updat/theme/chips/floating_with_silent_download.dart';
-import 'package:updat/updat.dart';
-
 import 'package:http/http.dart' as http;
 import 'package:updat/updat_window_manager.dart';
 import 'src/version.dart' as version;
@@ -45,19 +43,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final (bool, String) _environmentStatus = checkEnvironment();
 
-  Future<String> _getLatestVersion() async {
-    // Github gives us a super useful latest endpoint, and we can use it to get the latest stable release
-    final data = await http.get(Uri.parse(
-      "https://api.github.com/repos/$kGitHubProject/releases/latest",
-    ));
-
-    // Return the tag name, which is always a semantically versioned string.
-    return jsonDecode(data.body)["tag_name"];
-  }
-
   Widget _buildNextButton() {
     return Directionality(
-      textDirection: TextDirection.rtl, child: TextButton.icon(
+      textDirection: TextDirection.rtl,
+      child: TextButton.icon(
         onPressed: _environmentStatus.$1 ? () => context.navigatePage((context) => const FillInPage()) : null,
         style: textButtonStyle(context),
         icon: const Icon(Icons.navigate_before),
@@ -72,10 +61,9 @@ class _MyHomePageState extends State<MyHomePage> {
     return UpdatWindowManager(
       appName: widget.title,
       currentVersion: version.packageVersion,
-      getLatestVersion: _getLatestVersion,
-      getBinaryUrl: (latestVersion) async {
-        return "https://github.com/$kGitHubProject/releases/download/$latestVersion/video_cutter-${Platform.operatingSystem}-$latestVersion.zip";
-      },
+      getLatestVersion: getLatestVersion,
+      getBinaryUrl: getLatestBinary,
+      getChangelog: getChangeLog,
       updateChipBuilder: floatingExtendedChipWithSilentDownload,
       child: Scaffold(
         appBar: AppBar(
--- lib/util/config.dart
+++ lib/util/config.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'dart:io';
-import 'package:path/path.dart' as path;
 import 'package:uuid/uuid.dart';
+import 'package:path/path.dart' as path;
+import 'package:http/http.dart' as http;
 
 final Directory workingDirectory = Directory('temp')..createSync();
 final Directory downloadDirectory = Directory(path.join(workingDirectory.path, 'downloaded'))..createSync();
@@ -10,5 +12,27 @@ final Directory dataDirectory = Directory(path.join(workingDirectory.path, 'data
 
 const Uuid kUUID = Uuid();
 
-
 const String kGitHubProject = 'callisto-jovy/video_cutter_ui';
+
+Future<String> getLatestVersion() async {
+  // Github gives us a super useful latest endpoint, and we can use it to get the latest stable release
+  final data = await http.get(Uri.parse(
+    "https://api.github.com/repos/$kGitHubProject/releases/latest",
+  ));
+
+  // Return the tag name, which is always a semantically versioned string.
+  return jsonDecode(data.body)["tag_name"];
+}
+
+// TODO: Other operating systems than windows.
+Future<String> getLatestBinary(final String? latestVersion) async {
+  return "https://github.com/$kGitHubProject/releases/download/$latestVersion/video_cutter-${Platform.operatingSystem}-$latestVersion.zip";
+}
+
+Future<String> getChangeLog(final String _, final String __) async {
+  // That same latest endpoint gives us access to a markdown-flavored release body. Perfect!
+  final data = await http.get(Uri.parse(
+    "https://api.github.com/repos/$kGitHubProject/releases/latest",
+  ));
+  return jsonDecode(data.body)["body"];
+}
