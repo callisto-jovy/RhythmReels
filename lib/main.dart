49bb6d07a13c3f0169ab2020a68419d2f9e41657
yugata <romanportius@gmail.com>
2024-03-27 20:32:33 +0100

    Update version internally & use other updateChipBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:future_debounce_button/future_debounce_button.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:updat/theme/chips/flat_with_check_for.dart';
 import 'package:updat/theme/chips/floating_with_silent_download.dart';
 import 'package:updat/updat_window_manager.dart';
 import 'src/version.dart' as version;
@@ -73,7 +75,8 @@ class _MyHomePageState extends State<MyHomePage> {
       getLatestVersion: getLatestVersion,
       getBinaryUrl: getLatestBinary,
       getChangelog: getChangeLog,
-      updateChipBuilder: floatingExtendedChipWithSilentDownload,
+      updateChipBuilder: flatChipWithCheckFor,
+      closeOnInstall: true,
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
