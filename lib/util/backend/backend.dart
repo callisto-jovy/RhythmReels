aa9fd5fc4fc1f08e26e85f23bed22f434863f6f6
yugata <romanportius@gmail.com>
2024-04-05 16:37:15 +0200

    Format backend with max-line=120

--- lib/util/backend/backend.dart
+++ lib/util/backend/backend.dart
@@ -27,16 +27,11 @@ Future<File> initBackend() async {
   // write byte data into the applications document directory.
   final ByteData byteData = await rootBundle.load('assets/$kCuttingRunnable');
 
-  return await file
-      .writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+  return await file.writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
 }
 
 Stream<String> runCutting(
-    {required String audioPath,
-    required String outputPath,
-    required String videosPath,
-    required List<double> beatTimes,
-    File? imageOverlay}) async* {
+    {required String audioPath, required String outputPath, required String videosPath, required List<double> beatTimes, File? imageOverlay}) async* {
   final List<String> beats = [];
   for (final double value in beatTimes) {
     beats.add('--beat');
@@ -44,8 +39,8 @@ Stream<String> runCutting(
   }
 
   // Custom ffmpeg location
-  final Directory? ffmpegPlatform = await FFMpegHelper().getPlatformFFMpeg();
   final bool ffmpegPresent = await FFMpegHelper().localInstallPerformed();
+  final Directory? ffmpegPlatform = await FFMpegHelper().getPlatformFFMpeg();
 
   // Load the cutter from the root bundle
   final File backend = await initBackend();
@@ -62,10 +57,7 @@ Stream<String> runCutting(
       '--output',
       outputPath,
       if (ffmpegPresent && ffmpegPlatform != null) ...['--ffmpeg', ffmpegPlatform.path],
-      if (imageOverlay != null) ...[
-        '--image_overlay',
-        imageOverlay.path
-      ], // love dart for allowing spreads like this :)
+      if (imageOverlay != null) ...['--image_overlay', imageOverlay.path], // love dart for allowing spreads like this :)
       ...beats
     ],
   );
@@ -76,8 +68,7 @@ Stream<String> runCutting(
   yield* process.stdout.transform(utf8.decoder);
 
   // Potential error log.
-  final List<String> errorLog =
-      await process.stderr.transform(utf8.decoder).asyncMap((event) => 'Error: $event').toList();
+  final List<String> errorLog = await process.stderr.transform(utf8.decoder).asyncMap((event) => 'Error: $event').toList();
 
   for (final String error in errorLog) {
     yield 'Error: $error';
